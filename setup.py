373b4f57636445d793f872f38f0d12bffbd09aff
Prakhar Dixit <prakhardixit2k17@gmail.com>
2024-12-11 22:52:40 +0530

    Files Created

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages,setup
+
+
+setup(
+    name='mlproject',
+    version = '0.0.1',
+    author = 'Prakhar',
+    author_email='prakhardixit2k17@gmail.com',
+    packages=find_packages()
+    install_requires=['pandas','numpy','matplotlib','seaborn','scikit-learn','mlflow','streamlit','fastapi']
+    
+    
+    
+    
+    
+)
